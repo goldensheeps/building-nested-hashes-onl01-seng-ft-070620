9859565bcd98e702d21b3262745d3b650a703cae
Timothy Lalli <trlalli92@gmail.com>
2020-07-19 15:05:43 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {:name => "Benvolio",
         :age => 17,
         :attitude => "worried",
-        }
+        },
         
         {:name => "Mercutio",
         :age => 18,
@@ -25,7 +25,7 @@ def fourth_challenge
         {:name => "Steven",
         :age => 30,
         :attitude => "confused",
-        }
+        },
         
         {:name => "Nurse",
         :age => 44,
